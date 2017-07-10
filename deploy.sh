76211e5d13e88a3f7e241c4b32c9067fe796da93
Stephen Sugden <me@stephensugden.com>
2017-07-10 15:26:49 +0200

    Fixup command in deploy.sh to match what's in actual docker image

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ kubectl --cluster=default --user=default apply -f "$PLUGIN_RESOURCE_FILE"
 if [ -n "$PLUGIN_INGRESS_TEMPLATE" ]; then
    touch ingress.yml
    sleep 5
-   SVCS=($(kubectl --cluster=default --user=default get svc -o json | jq -r .items[].metadata.name))
+   SVCS=($(kubectl --cluster=default --user=default get svc -o json | jq -r '.items[].metadata.labels.branch | select(. != null)'))
    echo "services: ${SVCS[@]}"
    "$PLUGIN_INGRESS_TEMPLATE" ${SVCS[@]} >> ingress.yml
 fi
